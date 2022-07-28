241e4c92c8f338bde4a95ecf77af6280cf8ee523
Matias Garrido <matias17garrido@gmail.com>
2022-07-28 18:39:02 -0300

    home-div-image

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import './Home.css'
-
+import React from "react";
+import "./Home.css";
 
 export const Home = () => {
   return (
-    <div className='banner-home'>
-        <div className='container-home'>
+    <div>
+      <div className="padding">
+        <div className="banner-home">
+          <div className="container-home">
+            <div className="sq-withe-cont">
+              <p className="text-home">
+                Cuidamos la calidad en cada paso para llevar lo mejor a tu mesa
+              </p>
+              <button className="button-con-home">CONOCENOS</button>
+            </div>
+          </div>
         </div>
+      </div>
+      <div className="div-pep-home">
+        <div className="pep-bg-black">
+          <p className="nosotros-title-home">Sobre Nosotros</p>
+          <p className="nosotros-pep-home">
+            HorVinDul S.R.L. es una empresa que crece día a día gracias a su
+            calidad de productos y fieles clientes, lo invitamos a degustarlos y
+            así sumarse a los que confían en nuestra calidad y sabor.
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
